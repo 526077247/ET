41ed3459572103d1916a5bc220192194539efa5c
mayuntao <526077247@qq.com>
2021-11-09 19:04:15 +0800

    多语言整理

---
+++ Unity/Assets/Model/Module/I18N/I18NComponent.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ET
+{
+    public class I18NComponent : Entity
+    {
+        public static I18NComponent Instance;
+        //”Ô—‘¿‡–Õ√∂æŸ
+        public enum LangType
+        {
+            Chinese,
+            English,
+        }
+        public LangType curLangType;
+        public readonly Dictionary<int, I18NConfig> i18nTextDic = new Dictionary<int, I18NConfig>();
+        public readonly Dictionary<string, I18NConfig> i18nTextKeyDic = new Dictionary<string, I18NConfig>();
+    }
+
+}
---
+++ Unity/Assets/Mono/I18N/I18NBridge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class I18NBridge
+{
+
+    public static I18NBridge Instance { get; private set; } = new I18NBridge();
+
+    public Func<string, string> GetValueByKey;
+
+    public Func<int, string> GetValueById;
+
+    /// <summary>
+    /// 通过中文本获取多语言文本(还没实现,先用根据ID获取的重载)
+    /// </summary>
+    /// <param name="str">中文文本</param>
+    /// <returns></returns>
+    public string GetText(string str)
+    {
+        return GetValueByKey?.Invoke(str);
+    }
+
+    /// <summary>
+    /// 通过文本ID获取多语言文本
+    /// </summary>
+    /// <param name="id">文本ID</param>
+    /// <returns></returns>
+    public string GetText(int id)
+    {
+        return GetValueById?.Invoke(id);
+    }
+}
+
