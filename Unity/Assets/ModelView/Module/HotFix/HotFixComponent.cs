bdbe12e471f5ad24ce80bc3d88c7baebb601a1e0
mayuntao <526077247@qq.com>
2021-11-17 19:40:45 +0800

    修改几个小问题

--- Unity/Assets/ModelView/Module/HotFix/HotFixComponent.cs
+++ Unity/Assets/ModelView/Module/HotFix/HotFixComponent.cs
@@ -9,13 +9,6 @@ namespace ET
     public class HotFixComponent:Entity
     {
         public static HotFixComponent Instance;
-        public string[] Assemblys = new string[]
-        {
-            "Assembly-CSharp",
-            "Unity.Hotfix",
-            "Unity.HotfixView",
-            "Unity.Model",
-            "Unity.ModelView",
-        };
+        public string[] Assemblys;
     }
 }
