9797951c2146abdd661916b3e899661e12fb74cb
mayuntao <526077247@qq.com>
2021-11-24 14:36:08 +0800

    Merge

--- Unity/Codes/HotfixView/AppStart_Init.cs
+++ Unity/Codes/HotfixView/AppStart_Init.cs
@@ -24,19 +24,19 @@ namespace ET
             Game.Scene.AddComponent<ConfigComponent>();
             ConfigComponent.Instance.Load();
             
-            // Game.Scene.AddComponent<I18NComponent>();
-            // Game.Scene.AddComponent<OpcodeTypeComponent>();
-            // Game.Scene.AddComponent<MessageDispatcherComponent>();
-            //
-            // Game.Scene.AddComponent<NetThreadComponent>();
-            // Game.Scene.AddComponent<SessionStreamDispatcher>();
-            //
-            // Game.Scene.AddComponent<ZoneSceneManagerComponent>();
-            //
-            // Game.Scene.AddComponent<GlobalComponent>();
-            // Game.Scene.AddComponent<AIDispatcherComponent>();
+            Game.Scene.AddComponent<I18NComponent>();
+            Game.Scene.AddComponent<OpcodeTypeComponent>();
+            Game.Scene.AddComponent<MessageDispatcherComponent>();
             
-            // await UIManagerComponent.Instance.OpenWindow<UIUpdateView>();//下载热更资源
+            Game.Scene.AddComponent<NetThreadComponent>();
+            Game.Scene.AddComponent<SessionStreamDispatcher>();
+            
+            Game.Scene.AddComponent<ZoneSceneManagerComponent>();
+            
+            Game.Scene.AddComponent<GlobalComponent>();
+            Game.Scene.AddComponent<AIDispatcherComponent>();
+            
+            await UIManagerComponent.Instance.OpenWindow<UIUpdateView>();//下载热更资源
             await ETTask.CompletedTask;
         }
     }
