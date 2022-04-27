91ccb32b6627f14d08d3da0569ddfc0b6bfce082
mayuntao <45992829+526077247@users.noreply.github.com>
2022-04-27 23:18:47 +0800

    修复单位obj回收没通过池子

--- Unity/Codes/HotfixView/Demo/Unit/AfterUnitCreate_CreateUnitView.cs
+++ Unity/Codes/HotfixView/Demo/Unit/AfterUnitCreate_CreateUnitView.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace ET
 {
@@ -12,7 +13,10 @@ namespace ET
             var go = await GameObjectPoolComponent.Instance.GetGameObjectAsync(args.Unit.Config.Perfab);
             go.transform.position = args.Unit.Position;
             go.transform.parent = GlobalComponent.Instance.Unit;
-            args.Unit.AddComponent<GameObjectComponent>().GameObject = go;
+            args.Unit.AddComponent<GameObjectComponent,GameObject,Action>(go, () =>
+            {
+                GameObjectPoolComponent.Instance?.RecycleGameObject(go);
+            });
             args.Unit.AddComponent<AnimatorComponent>();
         }
     }
