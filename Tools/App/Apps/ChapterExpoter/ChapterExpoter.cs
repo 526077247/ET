c091fc413dead7524ba895af2e8b9016d295aeea
Tailor <45992829+526077247@users.noreply.github.com>
2022-07-01 22:55:49 +0800

    修复导表BUG

--- Tools/App/Apps/ChapterExpoter/ChapterExpoter.cs
+++ Tools/App/Apps/ChapterExpoter/ChapterExpoter.cs
@@ -52,7 +52,7 @@ namespace ET
                 string fileName = Path.GetFileName(excelPath);
                 if (!fileName.EndsWith(".xlsx") || fileName.StartsWith("~$") || fileName.Contains("#"))
                 {
-                    return;
+                    continue;
                 }
 
                 string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
