30a700852dd4726a62dfed26ffa8a95156060afd
mayuntao <mayuntao@bilibili.com>
2022-07-23 13:47:16 +0800

    修改buff字段

--- Unity/Codes/Model/Generate/Config/BuffBleedConfig.cs
+++ Unity/Codes/Model/Generate/Config/BuffBleedConfig.cs
@@ -82,24 +82,9 @@ namespace ET
 		/// <summary>Id</summary>
 		[ProtoMember(1)]
 		public int Id { get; set; }
-		/// <summary>计算方式</summary>
-		/// <summary> 0=固定值(非千分比)</summary>
-		/// <summary> 1=自身物理攻击力比例(千分比)</summary>
-		/// <summary> 2=自身魔法攻击力比例(千分比)</summary>
-		/// <summary> 3=自身物理防御力比例(千分比)</summary>
-		/// <summary> 4=自身魔法防御力比例(千分比)</summary>
-		/// <summary> 5=自身生命值上限比例(千分比)</summary>
-		/// <summary> 6=自身已损失生命值比例(千分比)</summary>
-		/// <summary> 7=自身当前失生命值比例(千分比)</summary>
-		/// <summary> 8=目标物理攻击力比例(千分比)</summary>
-		/// <summary> 9=目标魔法攻击力比例(千分比)</summary>
-		/// <summary> 10=目标物理防御力比例(千分比)</summary>
-		/// <summary> 11=目标魔法防御力比例(千分比)</summary>
-		/// <summary> 12=目标生命值上限比例(千分比)</summary>
-		/// <summary> 13=目标已损失生命值比例(千分比)</summary>
-		/// <summary> 14=目标当前失生命值比例(千分比)</summary>
+		/// <summary>计算公式</summary>
 		[ProtoMember(2)]
-		public int OperateType { get; set; }
+		public int FormulaId { get; set; }
 		/// <summary>出血间隔</summary>
 		[ProtoMember(3)]
 		public int CD { get; set; }
